36999721a7d021049f3e5401ecbd52df363473a2
Matt DeCamp <matt@mattdecamp.com>
2023-04-17 21:53:27 -0400

    removed boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import TabList from './components/TabList'
+import PacePanel from './components/panels/PacePanel'
+// import ConvertPanel from './components/panels/ConvertPanel'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="wrapper">
+        <TabList />
+        <PacePanel />
+        {/* <ConvertPanel /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
