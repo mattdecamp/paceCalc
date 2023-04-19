70d04c4ede36c3dda1578572dd971bf4036c4ba5
Matt DeCamp <matt@mattdecamp.com>
2023-04-18 21:28:25 -0400

    added complete pace calculation functionality

--- src/components/panels/PacePanel.jsx
+++ src/components/panels/PacePanel.jsx
@@ -4,55 +4,65 @@ import Button from "../Button";
 import PaceOutput from "../PaceOutput";
 
 export default function PacePanel() {
-  function pacePerMile() {
-    // parseFloat ensures decimal input option
-    const hours = parseInt(document.runCalculator.hours.value),
-      minutes = parseInt(document.runCalculator.minutes.value),
-      seconds = parseInt(document.runCalculator.seconds.value),
-      mileage = parseFloat(document.runCalculator.mileage.value),
-      // secondsPerMile combines the seconds, minutes and hours into total seconds and divides that by mileage to get seconds per mile
-      secondsPerMile = (seconds + minutes * 60 + hours * 60 * 60) / mileage,
-      // Calculates the number of hours per mile;
-      paceHours = Math.floor(secondsPerMile / 3600),
-      timeLeft = secondsPerMile - paceHours * 3600,
-      // Converts secondsPerMile to a minutes per mile in decimal form
-      minutesDecimal = timeLeft / 60,
-      // From minutesDecimal assigns everything left of the decimal point to left
-      paceMinutes = Math.floor(minutesDecimal),
-      // From minutesDecimal assigns everything right of the decimal point to rightDecimal
-      paceSeconds = Math.round((minutesDecimal % 1) * 60);
-    // Calculate based on distance measurement
-
-    // Adds zeros to each field if needed
-    function zeroes(string, pad, length) {
-      return (new Array(length + 1).join(pad) + string).slice(-length);
-    }
-    const pace =
-      zeroes(paceHours, "0", 2) +
-      ":" +
-      zeroes(paceMinutes, "0", 2) +
-      ":" +
-      zeroes(paceSeconds, "0", 2);
-    // Replace error output with zeros
-    if (pace === "aN:aN:aN") {
-      document.runCalculator.result.value = "00:00:00";
-    } else {
-      document.runCalculator.result.value = pace;
-    }
-  }
-
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [mileage, setMileage] = useState(0);
 
   function handlePaceCalc() {
-    const currentHours = document.runCalculator.hours.value,
-      currentMinutes = document.runCalculator.minutes.value,
-      currentSeconds = document.runCalculator.seconds.value,
-      currentMileage = document.runCalculator.mileage.value;
+    /* 
+    / declare current input fields
+    / if they are blank, default their values to zero,
+    / with the exception of mileage, which cannot be zero
+    */
+    const currentHours =
+        document.runCalculator.hours.value == ""
+          ? 0
+          : parseFloat(document.runCalculator.hours.value),
+      currentMinutes =
+        document.runCalculator.minutes.value == ""
+          ? 0
+          : parseFloat(document.runCalculator.minutes.value),
+      currentSeconds =
+        document.runCalculator.seconds.value == ""
+          ? 0
+          : parseFloat(document.runCalculator.seconds.value),
+      currentMileage =
+        document.runCalculator.mileage.value == ""
+          ? alert("Mileage cannot be zero") // TODO turn this into warning
+          : parseFloat(document.runCalculator.mileage.value),
+      // calculate seconds per mile
+      secondsPerMile =
+        (currentSeconds + currentMinutes * 60 + currentHours * 60 * 60) /
+        currentMileage,
+      // calculate the number of hours per mile;
+      paceHours = Math.floor(secondsPerMile / 3600),
+      timeLeft = secondsPerMile - paceHours * 3600,
+      // convert secondsPerMile to a minutes per mile in decimal form
+      minutesDecimal = timeLeft / 60,
+      // From minutesDecimal assigns everything left of the decimal point to left
+      paceMinutes = Math.floor(minutesDecimal),
+      // From minutesDecimal assigns everything right of the decimal point to rightDecimal
+      paceSeconds = Math.round((minutesDecimal % 1) * 60);
 
+    // add zeros to each field if needed
+    function zeroes(string, pad, length) {
+      return (new Array(length + 1).join(pad) + string).slice(-length);
+    }
+    const pace =
+      zeroes(paceHours, 0, 2) +
+      ":" +
+      zeroes(paceMinutes, 0, 2) +
+      ":" +
+      zeroes(paceSeconds, 0, 2);
+    // replace error output with zeros
+    if (pace === "aN:aN:aN") {
+      document.runCalculator.result.value = "00:00:00";
+    } else {
+      document.runCalculator.result.value = pace;
+    }
     console.log(currentHours, currentMinutes, currentSeconds, currentMileage);
+    console.log("Pace: " + paceHours, paceMinutes, paceSeconds);
   }
 
   function handleReset() {
@@ -78,7 +88,6 @@ export default function PacePanel() {
             onChange={(e) => setHours(e.target.value)}
             labelName="Hours"
           />
-          <div>{hours}</div>
           <PaceInput
             paceInput="minutes"
             value={minutes}
